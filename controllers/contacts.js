a5dbf9e599140a8670dfb0be480e2286558e47d2
Roma <r.dutkevich@gmail.com>
2023-05-19 19:41:09 +0300

    added functionality with data base and wrote route to update user field favorite

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,16 +1,17 @@
 const { HttpError } = require("../helpers");
 const { ctrlWrapper } = require("../decorators");
-const contactsService = require("../models/contacts");
-const { contactsSchema } = require("../schemas");
+const { Contact } = require("../models/contact");
+const { schemas } = require("../models/contact");
 
 const getAll = async (_, res) => {
-  const result = await contactsService.listContacts();
+  const result = await Contact.find();
   res.json(result);
 };
 
 const getById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contactsService.getContactById(contactId);
+
+  const result = await Contact.findById(contactId);
 
   if (!result) throw HttpError(404);
 
@@ -18,18 +19,19 @@ const getById = async (req, res) => {
 };
 
 const addNewContact = async (req, res) => {
-  const { error } = contactsSchema.add.validate(req.body);
+  const { error } = schemas.addContact.validate(req.body);
 
   if (error) throw HttpError(400, "missing required name field");
 
-  const result = await contactsService.addContact(req.body);
+  const result = await Contact.create(req.body);
 
   res.status(201).json(result);
 };
 
 const removeById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contactsService.removeContact(contactId);
+
+  const result = await Contact.findByIdAndRemove(contactId);
 
   if (!result) throw HttpError(404);
 
@@ -38,11 +40,30 @@ const removeById = async (req, res) => {
 
 const updateById = async (req, res) => {
   const { contactId } = req.params;
-  const { error } = contactsSchema.update.validate(req.body);
+
+  const { error } = schemas.updateContact.validate(req.body);
 
   if (error) throw HttpError(400, "missing required name field");
 
-  const result = await contactsService.updateContact(contactId, req.body);
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+
+  if (!result) throw HttpError(404);
+
+  res.json(result);
+};
+
+const updateStatusContact = async (req, res) => {
+  const { contactId } = req.params;
+
+  const { error } = schemas.updateFavorite.validate(req.body);
+
+  if (error) throw HttpError(400, "missing required name field");
+
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
 
   if (!result) throw HttpError(404);
 
@@ -55,4 +76,5 @@ module.exports = {
   addNewContact: ctrlWrapper(addNewContact),
   removeById: ctrlWrapper(removeById),
   updateById: ctrlWrapper(updateById),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
 };
